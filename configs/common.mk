0cff0147d5fe23ab377358c1f80c365e873c62d2
Kristofer Ard <kristoferard@gmail.com>
2013-10-16 23:47:09 -0500

    Add superuser/ Add optional packages Signed-off-by: Kristofer Ard <kristoferard@gmail.com>

--- configs/common.mk
+++ configs/common.mk
@@ -3,6 +3,11 @@ PRODUCT_NAME := aospx
 PRODUCT_BRAND := aospx
 PRODUCT_DEVICE := generic
 
+# SuperUser
+SUPERUSER_EMBEDDED := true
+SUPERUSER_PACKAGE_PREFIX := com.android.settings.cyanogenmod.superuser
+
+# Overrides
 PRODUCT_PROPERTY_OVERRIDES += \
     keyguard.no_require_sim=true \
     ro.url.legal=http://www.google.com/intl/%s/mobile/android/basic/phone-legal.html \
@@ -11,7 +16,16 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.com.android.wifi-watchlist=GoogleGuest \
     ro.setupwizard.enterprise_mode=1 \
     ro.com.android.dateformat=MM-dd-yyyy \
-    ro.com.android.dataroaming=false
+    ro.com.android.dataroaming=false \
+    persist.sys.root_access=3
+    
+# Optional Packages
+PRODUCT_PACKAGES += \
+    BlueBalls \
+    SunBeam \
+    Superuser \
+    su \
+    Torch
 
 # Common overlay
 PRODUCT_PACKAGE_OVERLAYS += vendor/aospx/overlay/common
@@ -23,7 +37,7 @@ PRODUCT_PACKAGE_OVERLAYS += vendor/aospx/overlay/dictionaries
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml
 
-# system/lib
+# System/lib
 PRODUCT_COPY_FILES += \
     vendor/aospx/proprietary/common/system/lib/libjni_eglfence.so:system/lib/libjni_eglfence.so \
     vendor/aospx/proprietary/common/system/lib/libjni_filtershow_filters.so:system/lib/libjni_filtershow_filters.so \
