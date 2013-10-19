72c5bfdc6633f4516509108e3722cf40d8d85795
Kristofer Ard <kristoferard@gmail.com>
2013-10-18 21:51:38 -0500

    Remove libs Signed-off-by: Kristofer Ard <kristoferard@gmail.com>

--- configs/common.mk
+++ configs/common.mk
@@ -37,12 +37,6 @@ PRODUCT_PACKAGE_OVERLAYS += vendor/aospx/overlay/dictionaries
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml
 
-# System/lib
-PRODUCT_COPY_FILES += \
-    vendor/aospx/proprietary/common/system/lib/libjni_eglfence.so:system/lib/libjni_eglfence.so \
-    vendor/aospx/proprietary/common/system/lib/libjni_filtershow_filters.so:system/lib/libjni_filtershow_filters.so \
-    vendor/aospx/proprietary/common/system/lib/libjni_mosaic.so:system/lib/libjni_mosaic.so
-
 # Versioning System
 RELEASE = false
 AOSPX_VERSION_MAJOR = 1
